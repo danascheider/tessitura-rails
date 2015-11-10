a1c1c8a2e01c91cf28e4a87034426eb80583735e
Dana Scheider <dana.scheider@gmail.com>
2015-11-10 14:39:42 -0800

    More components to make homepage features pass

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -10,6 +10,10 @@ Then /^I should see the top navbar$/ do
   expect(page).to have_selector '#navbar-top'
 end
 
+Then /^I should see the (\S*) view$/ do |id|
+  expect(page).to have_selector "##{id}"
+end
+
 Then /^I should see a link to the '([^']*)' view$/ do |title|
   expect(page).to have_selector "a[title='#{title}']"
 end
