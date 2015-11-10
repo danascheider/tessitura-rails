18e7601662d9a4ac64803f57f4d97938dbadae3b
Dana Scheider <dana.scheider@gmail.com>
2015-11-10 14:20:07 -0800

    Passing specs for the homepage

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -1,3 +1,11 @@
 Given /^I am on the homepage$/ do 
   visit '/'
 end
+
+Then /^I should see the top navbar$/ do 
+  expect(page).to have_selector '#navbar-top'
+end
+
+Then /^I should see a link to the '([^']*)' view$/ do |title|
+  expect(page).to have_selector "a[title='#{title}']"
+end
