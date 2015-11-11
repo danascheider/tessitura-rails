792c44703278e648ae3a700e58a98b3bee818866
Dana Scheider <dana.scheider@gmail.com>
2015-11-10 21:00:42 -0800

    Move user-related step into user step definition file

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -6,21 +6,6 @@ When /^I click the '([^']*)' link$/ do |link|
   click_link link
 end
 
-When /^I submit the registration form with valid attributes$/ do 
-  within '#new_user' do 
-    fill_in 'user[username]', with: 'newuser2016'
-    fill_in 'user[password]', with: '2016usernew'
-    fill_in 'user[password_confirmation]', with: '2016usernew'
-    fill_in 'user[email]', with: 'newuser2016@example.com'
-    fill_in 'user[email_confirmation]', with: 'newuser2016@example.com'
-    fill_in 'user[first_name]', with: 'Frank'
-    fill_in 'user[last_name]', with: 'Zappa'
-    check 'user[terms]'
-
-    click_button 'Create Account'
-  end
-end
-
 Then /^I should see the top navbar$/ do 
   expect(page).to have_selector '#navbar-top'
 end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,6 +2,21 @@ Given /^there are no users$/ do
   User.count == 0
 end
 
+When /^I submit the registration form with valid attributes$/ do 
+  within '#new_user' do 
+    fill_in 'user[username]', with: 'newuser2016'
+    fill_in 'user[password]', with: '2016usernew'
+    fill_in 'user[password_confirmation]', with: '2016usernew'
+    fill_in 'user[email]', with: 'newuser2016@example.com'
+    fill_in 'user[email_confirmation]', with: 'newuser2016@example.com'
+    fill_in 'user[first_name]', with: 'Frank'
+    fill_in 'user[last_name]', with: 'Zappa'
+    check 'user[terms]'
+
+    click_button 'Create Account'
+  end
+end
+
 Then /^there should be (\d+) users?$/ do |count|
   expect(User.count).to eql count.to_i
 end
