8258648408b6903fb6326942d17c2cb038f5a933
Dana Scheider <dana.scheider@gmail.com>
2015-11-27 17:28:40 -0800

    Add association between users and tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,10 @@
 class Task < ActiveRecord::Base
+  belongs_to :user
+  acts_as_list scope: :user
+  
   before_validation :set_priority
 
+  validates_presence_of :title
   validates :status, inclusion: { in: ["In Progress", "Blocking", "Complete"], allow_nil: true }
   validates :priority, inclusion: { in: ["Urgent", "High", "Normal", "Low", "Not Important"] }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   validates_presence_of :first_name, :last_name, :email
   validates :email, confirmation: true
   validates_acceptance_of :terms
+
+  has_many :tasks, -> { order(position: :asc) }, dependent: :destroy
 
   def full_name
     "#{first_name} #{last_name}"
