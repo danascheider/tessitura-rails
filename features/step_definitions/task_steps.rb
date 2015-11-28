22701df6e9ffb3000d5ade77466f01bdf0f3ed87
Dana Scheider <dana.scheider@gmail.com>
2015-11-27 17:41:57 -0800

    Define additional task step

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -4,3 +4,8 @@ Given /^I have 1 tasks? in each status category$/ do
   FactoryGirl.create(:task, user_id: @user.id, status: 'Blocking')
   FactoryGirl.create(:task, user_id: @user.id, status: 'Complete')
 end
+
+When /^I visit my tasks page$/ do 
+  visit "/users/#{@user.id}/tasks"
+end
+
