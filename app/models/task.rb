1d00a7942ef0b90a509c1e505bfc122fedf138a5
Dana Scheider <dana.scheider@gmail.com>
2015-11-29 16:04:52 -0800

    Change order of statements inside class

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,12 @@
-class Task < ActiveRecord::Base
-  belongs_to :user
-  acts_as_list scope: :user
-  
+class Task < ActiveRecord::Base 
   before_validation :set_priority
 
   validates_presence_of :title
   validates :status, inclusion: { in: ["In Progress", "Blocking", "Complete"], allow_nil: true }
   validates :priority, inclusion: { in: ["Urgent", "High", "Normal", "Low", "Not Important"] }
+
+  belongs_to :user
+  acts_as_list scope: :user
 
   private
     def set_priority
