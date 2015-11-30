22073788a25165ab793cf7d81b7c794be6242f9e
Dana Scheider <dana.scheider@gmail.com>
2015-11-29 16:12:22 -0800

    Add 'user_with_tasks' factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,15 @@ FactoryGirl.define do
     sequence :email do |n|
       "user2016#{n}@example.com"
     end
+
+    factory :user_with_tasks do 
+      transient do 
+        count 4
+      end
+
+      after(:create) do |user, evaluator|
+        FactoryGirl.create_list(:task, evaluator.count, :user_id => user.id)
+      end
+    end
   end
 end
