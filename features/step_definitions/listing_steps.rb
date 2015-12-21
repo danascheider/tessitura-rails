f6dea76c651322dbc343f1c12b3b3c9f9607ab17
Dana Scheider <dana.scheider@gmail.com>
2015-12-21 13:33:48 -0800

    Define another setup step for listing tests

--- features/step_definitions/listing_steps.rb
+++ features/step_definitions/listing_steps.rb
@@ -2,6 +2,10 @@ Given /^I am not logged in$/ do
   delete destroy_user_session_path
 end
 
+Given /^there are (\d+) listings$/ do |count|
+  FactoryGirl.create_list(:listing, count.to_i)
+end
+
 When /^I visit the listings page$/ do 
   visit listings_path
 end
