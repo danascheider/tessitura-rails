0f1675e3a0c69eeb00a3ac2f3fd7b22145c1d990
Dana Scheider <dana.scheider@gmail.com>
2015-12-22 11:14:21 -0800

    Use Warden::Test::Helpers to make sure tests pass even though login is required

--- spec/requests/listings_spec.rb
+++ spec/requests/listings_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Listings", type: :request do
+  include Warden::Test::Helpers
+
+  let(:admin) { FactoryGirl.create(:admin) }
+
+  before(:each) do 
+    Warden.test_mode!
+  end
+
+  after(:each) do 
+    Warden.test_reset!
+  end
+
   describe "GET /listings" do
     it "works! (now write some real specs)" do
+      login_as(admin, scope: :user, run_callbacks: false)
       get listings_path
       expect(response).to have_http_status(200)
     end
