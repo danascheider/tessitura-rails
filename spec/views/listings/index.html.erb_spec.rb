abcee9a98449e405a3f27d2da3d63370663e41df
Dana Scheider <dana.scheider@gmail.com>
2015-12-22 11:24:11 -0800

    Reimagine template for list of listings

--- spec/views/listings/index.html.erb_spec.rb
+++ spec/views/listings/index.html.erb_spec.rb
@@ -2,21 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "listings/index", type: :view do
   before(:each) do
-    assign(:listings, [
-      Listing.create!(
-        :title => "Title",
-        :description => "Description"
-      ),
-      Listing.create!(
-        :title => "Title",
-        :description => "Description"
-      )
-    ])
+    assign(:listings, FactoryGirl.create_list(:listing, 2))
   end
 
   it "renders a list of listings" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
+    assert_select ".listing", :count => Listing.count
   end
 end
